daa5232941070d72b81e229bb85dd540f5f41e43
Jarrod Nettles <jarrod@squarecrow.com>
2012-05-09 09:29:32 -0500

    Abstracted base functionality out of the main Phinq class and into a base class to support additional Phinq libraries.

---
+++ src/Phinq/PhinqBase.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Phinq;
+
+use IteratorAggregate, Closure, OutOfBoundsException, BadMethodCallException, InvalidArgumentException, ArrayIterator;
+
+abstract class PhinqBase implements \IteratorAggregate
+{
+	protected $collection;
+	protected $evaluatedCollection;
+	protected $queryQueue = array();
+	protected $index = 0;
+	protected $isDirty = false;
+	
+	/**
+	 * Construct a new instance of the Phinq object. You must call the create method
+	 * statically to actually initiate this constructor.
+	 * @param array|Phinq|Iterator|IteratorAggregate $collection The initial collection to query on
+	 */
+	protected function __construct($collection, array $queries = array())
+	{
+		$this->collection = Util::convertToNumericallyIndexedArray($collection);
+		$this->evaluatedCollection = $this->collection;
+	
+		if(!empty($queries))
+		{
+			foreach($queries as $query)
+			{
+				$this->addToQueue($query);
+			}
+		}
+	}
+	
+	/**
+	 * A short method for quickly constructing the Phinq object without the need for extra typing.
+	 * This method is a clone of the Phinq::create($collection) method.
+	 * @see Phinq::create
+	 * @param array|Phinq|Iterator|IteratorAggregate $collection The initial collection to query on.
+	 * @return \Phinq\Phinq
+	 */
+	public static function _($collection) {
+		return new static($collection);
+	}
+	
+	/**
+	 * Convenience factory method for method chaining
+	 * @deprecated This method is deprecated in favor of using the Phinq::_($collection) method.
+	 * @param array|Phinq|Iterator|IteratorAggregate $collection The initial collection to query on
+	 * @return Phinq
+	 */
+	public final static function create($collection) {
+		return new static($collection);
+	}
+	
+	/**
+	 * Executes any queries in the stack and with the option of running a final closure
+	 * similar to a where clause before returning the collection.
+	 * @param Closure $predicate
+	 */
+	protected function getCollection(Closure $predicate = null)
+	{
+		$collection = $this->toArray();
+	
+		if ($predicate !== null) {
+			$collection = self::create($collection)->where($predicate)->toArray();
+		}
+	
+		return $collection;
+	}
+	
+	/**
+	 * Adds a query to the Phinq query stack.
+	 * @param Query $query
+	 * @return void
+	 */
+	protected final function addToQueue(Query $query)
+	{
+		$this->queryQueue[] = $query;
+		$this->isDirty = true;
+	}
+	
+	/**
+	 * Returns the last query in the queue.
+	 * @return Ambigous <NULL, mixed>
+	 */
+	protected final function getLastQuery()
+	{
+		return empty($this->queryQueue) ? null : end($this->queryQueue);
+	}
+	
+	/**
+	 * Executes any queries in the stack and returns the collection as an array.
+	 * @return array
+	 */
+	public function toArray()
+	{
+		if($this->isDirty || $this->evaluatedCollection === null)
+		{
+			$this->index = 0;
+			$this->isDirty = false;
+			$this->evaluatedCollection = $this->collection;
+	
+			foreach($this->queryQueue as $query)
+			{
+				$this->evaluatedCollection = $query->execute($this->evaluatedCollection);
+			}
+		}
+	
+		return $this->evaluatedCollection;
+	}
+	
+	/**
+	 * Returns the collection as an ArrayAccess-able object, with the
+	 * keys being chosen using the given $keySelector
+	 *
+	 * @param Closure $keySelector A lambda function that takes one argument, the current element, and
+	 *                             returns a key for the dictionary entry for the corresponding element
+	 * @return Dictionary
+	 */
+	public function toDictionary(Closure $keySelector)
+	{
+		$collection = $this->toArray();
+	
+		$dictionary = new Dictionary();
+		for ($i = 0, $count = count($collection); $i < $count; $i++) {
+			$dictionary[$keySelector($collection[$i])] = $collection[$i];
+		}
+	
+		return $dictionary;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see IteratorAggregate::getIterator()
+	 */
+	public function getIterator()
+	{
+		return new ArrayIterator($this->toArray());
+	}
+}
